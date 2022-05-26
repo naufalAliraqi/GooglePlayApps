b745a6ccb2a236bb741fa72a53d510ac0d55b086
omar ezzdeen <omar.izziddeen@gmail.com>
2022-05-26 14:24:56 +0300

    add feature fined app google number one

--- src/main/kotlin/AppAnalyzer.kt
+++ src/main/kotlin/AppAnalyzer.kt
@@ -1,10 +1,29 @@
+import model.GooglePlayApp
+
 class AppAnalyzer {
 
-    fun finedAppDevelopedByGoogle(){
-
-
-
+    // region functions finedAppDevelopedByGoogle
+    /**
+     * @param googleApp This is a list of GooglePlayApp object
+     * @return This method returns a list of GooglePlayApp object specific count
+     * @author omar izziddeen
+     * @version 1.0
+     * @since 2022-05-26
+     * @see GooglePlayApp
+     * @see AppAnalyzer
+     *
+     */
+    fun finedAppDevelopedByGoogle(googleApp: MutableList<GooglePlayApp>): Int{
+        val listGoogleApps = mutableListOf<String>()
+        googleApp.forEach {
+            if (it.company.contains("Google")) {
+                listGoogleApps.add(it.appName)
+            }
+        }
+        return listGoogleApps.size
     }
+    // endregion
+
     fun finedPercentageOfMedicalApp(){
 
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,29 @@
 fun main() {
-//    val csvTest = CSVParser()
-//    csvTest.parseCSV()
+    val csvTest = CSVParser()
+    csvTest.parseCSV()
+
+    val appAnalyzer = AppAnalyzer()
+
+    // This code is just rudimentary and subject to modification
+    do {
+        println("1- how many apps were developed by Google in the dataset.")
+        println("2- the percentage of Medical apps. ")
+        println("3- what is the oldest app in the dataset. ")
+        println("4- the percentage of apps running on android 9 and up only. ")
+        println("5- what are the largest 10 apps in the dataset. ")
+        println("6- what are the top 10 installed apps. ")
+        println("7- exit")
+        println("Please enter the number of the app you want to know more about: ")
+        when (readLine()!!.toInt()) {
+            1 -> println("This is a develop by google " + appAnalyzer.finedAppDevelopedByGoogle(csvTest.googlePlayApps) + " apps" )
+            2 -> println("The percentage of Medical apps is " + appAnalyzer.finedPercentageOfMedicalApp() + "%")
+            3 -> println("The oldest app in the dataset is " + appAnalyzer.finedOldestApp())
+            4 -> println("The percentage of apps running on android 9 and up only is " + appAnalyzer.finedPercentageOfAppRunningOnAndroid9AndUp() + "%")
+            5 -> println("The largest 10 apps in the dataset are: " + appAnalyzer.finedLargest10App())
+            6 -> println("The top 10 installed apps are: " + appAnalyzer.finedTop10InstalledApp())
+            7 -> println("Goodbye")
+            else -> println("Please enter a valid number")
+        }
+    }while (readLine()!!.toInt() == 7)
+
 }
