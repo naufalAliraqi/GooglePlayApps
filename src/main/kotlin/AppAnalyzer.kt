9aab2e1d0b7df61934dcfe498f86784f2478d580
Nawfal Aliraqi <naufalxaliraqi1998@gmail.com>
2022-05-26 12:37:46 +0300

    add app analyzer class with header of the task function

---
+++ src/main/kotlin/AppAnalyzer.kt
@@ -0,0 +1,33 @@
+class AppAnalyzer {
+
+    fun finedAppDevelopedByGoogle(){
+
+
+
+    }
+    fun finedPercentageOfMedicalApp(){
+
+
+
+    }
+    fun finedOldestApp(){
+
+
+
+    }
+    fun finedPercentageOfAppRunningOnAndroid9AndUp(){
+
+
+
+    }
+    fun finedLargest10App(){
+
+
+
+    }
+    fun finedTop10InstalledApp(){
+
+
+
+    }
+}
