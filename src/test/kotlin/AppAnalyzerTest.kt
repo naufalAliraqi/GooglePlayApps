b83a66483dfcef2402f3ff19738e4c92357dfed8
Sadeq <dev.sadiqsalam@gmail.com>
2022-05-31 01:36:20 +0300

    prepare (apps developed by google) test cases

--- src/test/kotlin/AppAnalyzerTest.kt
+++ src/test/kotlin/AppAnalyzerTest.kt
@@ -1,21 +1,71 @@
-//import model.App
-//import org.junit.jupiter.api.Test
-//
-//import org.junit.jupiter.api.Assertions.*
-//import org.junit.jupiter.api.BeforeAll
-//import org.junit.jupiter.api.TestInstance
-//import java.time.LocalDate
-//import java.time.format.DateTimeFormatter
-//
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//internal class AppAnalyzerTest {
-//
-//    lateinit var appAnalyzer: AppAnalyzer
-//
-//    @BeforeAll
-//    fun setup() {
-//        appAnalyzer = AppAnalyzer()
-//    }
+import model.App
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class AppAnalyzerTest {
+
+    private lateinit var appAnalyzer: AppAnalyzer
+
+    @BeforeAll
+    fun setup() {
+        appAnalyzer = AppAnalyzer()
+    }
+    /* Points That Will Be Tested:
+    * 1. The apps that have been developed by Google.
+    * 2. The percentage of medical apps.
+    * 3. The oldest app.
+    * 4. The percentage of apps running on android 9 or above.
+    * 5. The largest 10 apps by size.
+    * 6. The top 10 installed apps.
+    * */
+
+    /// - THE NEW TEST CASES - ///
+
+    /*
+    * Point 1: The apps that have been developed by Google.
+    * No. of test cases: 8
+    * */
+
+    @Test // 1-1
+    fun should_ReturnNumOfAppsDevelopedByGoogle_When_CompanyNameContainsSpaces() {
+    }
+
+    @Test // 1-2
+    fun should_ReturnNumOfAppsDevelopedByGoogle_When_CompanyNameContainsDots() {
+    }
+
+    @Test // 1-3
+    fun should_ReturnNumOfAppsDevelopedByGoogle_When_CompanyNameInUpperCase() {
+    }
+
+    @Test // 1-4
+    fun should_ReturnNumOfAppsDevelopedByGoogle_When_CompanyNameInMixedCase() {
+    }
+
+    @Test // 1-5
+    fun should_ReturnNull_When_CompanyNameNotFound() {
+    }
+
+    @Test // 1-6
+    fun should_ReturnNull_When_CompanyNameIsEmpty() {
+    }
+
+    @Test // 1-7
+    fun should_ReturnNull_When_AppListIsEmpty() {
+    }
+
+    @Test // 1-8
+    fun should_ReturnNull_When_AppListIsNull() {
+    }
+
+
+
 //
 //    // region test functions for findAppDevelopedByGoogle function
 //    @Test
@@ -109,9 +159,10 @@
 //        )
 //        googlePlayAppList.add(
 //            App(
-//                "Eyes : Nonogram","GAMEFOX", "Puzzle",
+//                "Eyes : Nonogram", "GAMEFOX", "Puzzle",
 //                LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//                51.0,1500,5.0)
+//                51.0, 1500, 5.0
+//            )
 //        )
 //        // when calculate percentage
 //        val percentage = appAnalyzer.findPercentageOfAppsByCategory(googlePlayAppList)
@@ -390,18 +441,20 @@
 //        // when fined the largest10 app of the list
 //        val result = appAnalyzer.findLargestApps(googlePlayAppList)
 //        // then
-//        assertEquals(mutableListOf(
+//        assertEquals(
+//            mutableListOf(
 //                "Baby Game for 2 3 4 Year Old",
-//        "Crazy Pusher",
-//        "Dinosaur Airport:Game for kids",
-//        "StyleSeat: Book Hair & Beauty",
-//        "Eyes : Nonogram",
-//        "FOX 4 Dallas-Fort Worth: Weather",
-//        "Garage Master - games for kids",
-//        "Manta: Comics & Graphic Novels",
-//        "myAudi",
-//        "Slice: Pizza Delivery-Pick Up"
-//        ), result)
+//                "Crazy Pusher",
+//                "Dinosaur Airport:Game for kids",
+//                "StyleSeat: Book Hair & Beauty",
+//                "Eyes : Nonogram",
+//                "FOX 4 Dallas-Fort Worth: Weather",
+//                "Garage Master - games for kids",
+//                "Manta: Comics & Graphic Novels",
+//                "myAudi",
+//                "Slice: Pizza Delivery-Pick Up"
+//            ), result
+//        )
 //    }
 //
 //    @Test
@@ -461,7 +514,7 @@
 //    @Test
 //    fun should_ReturnNullValueAndNoLargestApp_When_TheListOfAppsIsEmpty() {
 //        // given empty list of google play apps
-//        val googlePlayAppList : MutableList<App> = mutableListOf()
+//        val googlePlayAppList: MutableList<App> = mutableListOf()
 //
 //        // when find the top installed apps name
 //        val result = appAnalyzer.findLargestApps(googlePlayAppList)
@@ -475,92 +528,190 @@
 //    @Test
 //    fun should_ReturnTop10InstalledAppName_When_TheListOfAppsIsCorrectAndContainsMoreThan9Apps() {
 //        // given list of google play apps have 15 element
-//        val googlePlayAppList : MutableList<App> = mutableListOf()
-//        googlePlayAppList.add(App("Jewel Blast : Temple", "SUPERBOX.Inc", "Puzzle",
-//            LocalDate.parse("April 11 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")), 50.0,1000,
-//            "4.4 and up"))
-//        googlePlayAppList.add(App("FOX 4 Dallas-Fort Worth: Weather", "Fox Television Stations Inc.",
-//            "Weather", LocalDate.parse("March 27 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            51.0,2000,"8.0 and up"))
-//        googlePlayAppList.add(App("Eyes : Nonogram","GAMEFOX", "Puzzle",
-//            LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            51.0,1500,"5.0 and up"))
-//        googlePlayAppList.add(App("Real Drift Car Racing","Real Games srls","Racing",
-//            LocalDate.parse("March 26 2021", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            51.0,500,"4.1 and up"))
-//        googlePlayAppList.add(App("Slice: Pizza Delivery-Pick Up","Slice Pizza App","Food & Drink",
-//            LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            51.0,2500,"7.0 and up"))
-//        googlePlayAppList.add(App("Angel Saga: Hero Action RPG","Alchemist Games Inc.","Action",
-//            LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            58.0,200,"5.0 and up"))
-//        googlePlayAppList.add(App("Crazy Pusher","Borg Studio","Casino",
-//            LocalDate.parse("March 25 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,3000,"4.1 and up"))
-//        googlePlayAppList.add(App("Baby Game for 2 3 4 Year Old","IDZ Digital Private Limited","Educational",
-//            LocalDate.parse("March 9 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,2800,"5.2 and up"))
-//        googlePlayAppList.add(App("Garage Master - games for kids","KIN GO GAMES FOR KIDS AND TODDLERS","Educational",
-//            LocalDate.parse("March 15 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,2600,"4.1 and up"))
-//        googlePlayAppList.add(App("Manta: Comics & Graphic Novels","RIDI Corporation","Comics",
-//            LocalDate.parse("May 16 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,2300,"5.0 and up"))
-//        googlePlayAppList.add(App("Sago Mini School (Kids 2-5)","Sago Mini","Education",
-//            LocalDate.parse("February 24 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,800,"4.4 and up"))
-//        googlePlayAppList.add(App("Rolling Luck: Win Real Money","Shape Keeper Ltd","Casino",
-//            LocalDate.parse("May 18 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,1100,"5.0 and up"))
-//        googlePlayAppList.add(App("StyleSeat: Book Hair & Beauty","Styleseat","Beauty",
-//            LocalDate.parse("May 18 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            54.0,2300,"5.1 and up"))
-//        googlePlayAppList.add(App("Dinosaur Airport:Game for kids","Yateland - Learning Games For Kids","Educational",
-//            LocalDate.parse("May 18 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            21.0,4000,"4.1 and up"))
-//        googlePlayAppList.add(App("myAudi","Audi","Auto & Vehicles",
-//            LocalDate.parse("May 10 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            53.0,1250,"8.0 and up"))
+//        val googlePlayAppList: MutableList<App> = mutableListOf()
+//        googlePlayAppList.add(
+//            App(
+//                "Jewel Blast : Temple", "SUPERBOX.Inc", "Puzzle",
+//                LocalDate.parse("April 11 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")), 50.0, 1000,
+//                "4.4 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "FOX 4 Dallas-Fort Worth: Weather", "Fox Television Stations Inc.",
+//                "Weather", LocalDate.parse("March 27 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                51.0, 2000, "8.0 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Eyes : Nonogram", "GAMEFOX", "Puzzle",
+//                LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                51.0, 1500, "5.0 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Real Drift Car Racing", "Real Games srls", "Racing",
+//                LocalDate.parse("March 26 2021", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                51.0, 500, "4.1 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Slice: Pizza Delivery-Pick Up", "Slice Pizza App", "Food & Drink",
+//                LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                51.0, 2500, "7.0 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Angel Saga: Hero Action RPG", "Alchemist Games Inc.", "Action",
+//                LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                58.0, 200, "5.0 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Crazy Pusher", "Borg Studio", "Casino",
+//                LocalDate.parse("March 25 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 3000, "4.1 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Baby Game for 2 3 4 Year Old", "IDZ Digital Private Limited", "Educational",
+//                LocalDate.parse("March 9 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 2800, "5.2 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Garage Master - games for kids", "KIN GO GAMES FOR KIDS AND TODDLERS", "Educational",
+//                LocalDate.parse("March 15 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 2600, "4.1 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Manta: Comics & Graphic Novels", "RIDI Corporation", "Comics",
+//                LocalDate.parse("May 16 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 2300, "5.0 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Sago Mini School (Kids 2-5)", "Sago Mini", "Education",
+//                LocalDate.parse("February 24 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 800, "4.4 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Rolling Luck: Win Real Money", "Shape Keeper Ltd", "Casino",
+//                LocalDate.parse("May 18 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 1100, "5.0 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "StyleSeat: Book Hair & Beauty", "Styleseat", "Beauty",
+//                LocalDate.parse("May 18 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                54.0, 2300, "5.1 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Dinosaur Airport:Game for kids", "Yateland - Learning Games For Kids", "Educational",
+//                LocalDate.parse("May 18 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                21.0, 4000, "4.1 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "myAudi", "Audi", "Auto & Vehicles",
+//                LocalDate.parse("May 10 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                53.0, 1250, "8.0 and up"
+//            )
+//        )
 //
 //        // when find the top 10 installed apps name
 //        val result = appAnalyzer.findTopNumberInstalledApps(googlePlayAppList)
 //        // then
-//        assertEquals(mutableListOf("Dinosaur Airport:Game for kids", "Crazy Pusher", "Baby Game for 2 3 4 Year Old", "Garage Master - games for kids",
-//            "Slice: Pizza Delivery-Pick Up", "Manta: Comics & Graphic Novels", "StyleSeat: Book Hair & Beauty", "FOX 4 Dallas-Fort Worth: Weather",
-//            "Eyes : Nonogram", "myAudi"), result)
+//        assertEquals(
+//            mutableListOf(
+//                "Dinosaur Airport:Game for kids",
+//                "Crazy Pusher",
+//                "Baby Game for 2 3 4 Year Old",
+//                "Garage Master - games for kids",
+//                "Slice: Pizza Delivery-Pick Up",
+//                "Manta: Comics & Graphic Novels",
+//                "StyleSeat: Book Hair & Beauty",
+//                "FOX 4 Dallas-Fort Worth: Weather",
+//                "Eyes : Nonogram",
+//                "myAudi"
+//            ), result
+//        )
 //    }
 //
 //    @Test
 //    fun should_ReturnAllElementAppName_When_TheListOfAppsIsCorrectAndContainsLiseThan10Apps() {
 //        // given list of google play apps have 5 element
-//        val googlePlayAppList : MutableList<App> = mutableListOf()
-//        googlePlayAppList.add(App("Slice: Pizza Delivery-Pick Up","Slice Pizza App","Food & Drink",
-//            LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            51.0,2500,"7.0 and up"))
-//        googlePlayAppList.add(App("Crazy Pusher","Borg Studio","Casino",
-//            LocalDate.parse("March 25 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,3000,"4.1 and up"))
-//        googlePlayAppList.add(App("Baby Game for 2 3 4 Year Old","IDZ Digital Private Limited","Educational",
-//            LocalDate.parse("March 9 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,2800,"5.2 and up"))
-//        googlePlayAppList.add(App("Garage Master - games for kids","KIN GO GAMES FOR KIDS AND TODDLERS","Educational",
-//            LocalDate.parse("March 15 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            52.0,2600,"4.1 and up"))
-//        googlePlayAppList.add(App("Dinosaur Airport:Game for kids","Yateland - Learning Games For Kids","Educational",
-//            LocalDate.parse("May 18 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
-//            21.0,4000,"4.1 and up"))
+//        val googlePlayAppList: MutableList<App> = mutableListOf()
+//        googlePlayAppList.add(
+//            App(
+//                "Slice: Pizza Delivery-Pick Up", "Slice Pizza App", "Food & Drink",
+//                LocalDate.parse("May 13 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                51.0, 2500, "7.0 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Crazy Pusher", "Borg Studio", "Casino",
+//                LocalDate.parse("March 25 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 3000, "4.1 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Baby Game for 2 3 4 Year Old", "IDZ Digital Private Limited", "Educational",
+//                LocalDate.parse("March 9 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 2800, "5.2 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Garage Master - games for kids", "KIN GO GAMES FOR KIDS AND TODDLERS", "Educational",
+//                LocalDate.parse("March 15 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                52.0, 2600, "4.1 and up"
+//            )
+//        )
+//        googlePlayAppList.add(
+//            App(
+//                "Dinosaur Airport:Game for kids", "Yateland - Learning Games For Kids", "Educational",
+//                LocalDate.parse("May 18 2022", DateTimeFormatter.ofPattern("MMMM d yyyy")),
+//                21.0, 4000, "4.1 and up"
+//            )
+//        )
 //
 //        // when find the top installed apps name
 //        val result = appAnalyzer.findTopNumberInstalledApps(googlePlayAppList)
 //        // then
-//        assertEquals(mutableListOf("Dinosaur Airport:Game for kids", "Crazy Pusher", "Baby Game for 2 3 4 Year Old", "Garage Master - games for kids",
-//            "Slice: Pizza Delivery-Pick Up"), result)
+//        assertEquals(
+//            mutableListOf(
+//                "Dinosaur Airport:Game for kids",
+//                "Crazy Pusher",
+//                "Baby Game for 2 3 4 Year Old",
+//                "Garage Master - games for kids",
+//                "Slice: Pizza Delivery-Pick Up"
+//            ), result
+//        )
 //    }
 //
 //    @Test
 //    fun should_ReturnNullValueAndNoTopInstalled_When_TheListOfAppsIsEmpty() {
 //        // given empty list of google play apps
-//        val googlePlayAppList : MutableList<App> = mutableListOf()
+//        val googlePlayAppList: MutableList<App> = mutableListOf()
 //
 //        // when find the top installed apps name
 //        val result = appAnalyzer.findTopNumberInstalledApps(googlePlayAppList)
@@ -569,4 +720,4 @@
 //    }
 //
 //    // endregion
-//}
+}
